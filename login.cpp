5a699c345f09a45a565574bcb67330b6ef2118b0
Daniel <daniel0803.felipe@gmail.com>
2023-06-13 22:32:42 -0500

    agrega la clase login

---
+++ login.cpp
@@ -0,0 +1,12 @@
+#include "std_lib_facilities.cpp"
+int main()
+{
+cout<<"1. ingrese su contraseña:\n";
+string contraseña = "";
+cin>> contraseña;
+
+cout<<"esta es su contraseña: "<<contraseña<<"\n";
+
+keep_window_open();
+return 0;
+}
