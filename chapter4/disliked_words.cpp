57d3732fbe2f015c763725f723f785bcc0b6829c
Daniel <daniel0803.felipe@gmail.com>
2023-06-27 16:58:36 -0500

    se crea el programa disliked_words.cpp funcional

---
+++ chapter4/disliked_words.cpp
@@ -0,0 +1,21 @@
+#include "std_lib_facilities.cpp"
+
+int main()
+{
+    string disliked = "ignorante";
+
+    for(string word; cin>>word; )
+    {
+        if (word != disliked)
+        {
+            cout<<word<<" ";
+        }
+        else
+        {
+            cout<<"beep ";
+        }
+
+    }
+    keep_window_open();
+    return 0;
+}
